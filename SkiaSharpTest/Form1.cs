cc75fe75a72a0dabe63e8b16cf7c7703209b4428
xwfh2000 <xwfh2009@yahoo.com>
2020-05-28 14:37:24 +0800

    call func DrawTextOnPath

--- SkiaSharpTest/Form1.cs
+++ SkiaSharpTest/Form1.cs
@@ -30,38 +30,55 @@ namespace SkiaSharpTest
         }
         private void Button1_Click(object sender, EventArgs e)
         {
-            var info = new SKImageInfo(256, 256);
+            var info = new SKImageInfo(512, 512);
             using (var surface = SKSurface.Create(info))
             {
                 SKCanvas canvas = surface.Canvas;
 
                 canvas.Clear(SKColors.White);
 
-                // configure our brush
-                var redBrush = new SKPaint
+               
+
+                var paint1 = new SKPaint
                 {
-                    Color = new SKColor(0xff, 0, 0),
-                    IsStroke = true,
-                    IsAntialias = true
+                    TextSize = 32.0f,
+                    IsAntialias = true,
+                    Color = new SKColor(255, 0, 0),
+                    Style = SKPaintStyle.Fill
                 };
-                var blueBrush = new SKPaint
+                var paint2 = new SKPaint
                 {
-                    Color = new SKColor(0, 0, 0xff),
-                    IsStroke = true,
-                    IsAntialias = true
+                    Style = SKPaintStyle.Stroke,
+                    PathEffect=SKPathEffect.CreateDiscrete(3,1)
                 };
 
+                var path = new SKPath();
+                path.AddArc(new SKRect(100, 100, 300, 300), 225, 90);
 
+                canvas.DrawPath(path, paint2);
+                canvas.DrawTextOnPath("跳-123", path, 0, 0, paint1);
 
-
-                for (int j = 0; j < 10000; j++)
-                {
-                    for (int i = 0; i < 64; i += 8)
-                    {
-                        var rect = new SKRect(i, i, 256 - i - 1, 256 - i - 1);
-                        canvas.DrawRect(rect, (i % 16 == 0) ? redBrush : blueBrush);
-                    }
-                }
+                //// configure our brush
+                //var redBrush = new SKPaint
+                //{
+                //    Color = new SKColor(0xff, 0, 0),
+                //    IsStroke = true,
+                //    IsAntialias = true
+                //};
+                //var blueBrush = new SKPaint
+                //{
+                //    Color = new SKColor(0, 0, 0xff),
+                //    IsStroke = true,
+                //    IsAntialias = true
+                //};
+                //for (int j = 0; j < 10000; j++)
+                //{
+                //    for (int i = 0; i < 64; i += 8)
+                //    {
+                //        var rect = new SKRect(i, i, 256 - i - 1, 256 - i - 1);
+                //        canvas.DrawRect(rect, (i % 16 == 0) ? redBrush : blueBrush);
+                //    }
+                //}
 
                 using (SKPaint textPaint = new SKPaint())
                 using (SKTypeface tf = SKTypeface.FromFamilyName("黑"))
